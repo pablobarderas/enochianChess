5cdb53afa73c8ffba8283f5acc159b92258776c6
Pablo <pbarderas31@gmail.com>
2024-01-23 21:54:23 +0100

    Add board image and set primary methods to show it

--- core/src/com/bardev/enochianchess/MainGame.java
+++ core/src/com/bardev/enochianchess/MainGame.java
@@ -1,31 +1,52 @@
 package com.bardev.enochianchess;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture board;
+
+	private int width, height;
+
+
+	// Upload files here
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		board = new Texture("tablero-naranja-notacion.png");
+
+		// Width x Height
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		System.out.println("My screen: " + width + "x" + height );
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
+	// Used to clear resources
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		board.dispose();
 	}
+
+	// Update game state, render files ...
+	@Override
+	public void render () {
+		// Clear buffer
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		// Begin draw
+		batch.begin();
+		batch.draw(board, 0, 0, 400, 400);
+		batch.end();
+
+	}
+
+
 }
